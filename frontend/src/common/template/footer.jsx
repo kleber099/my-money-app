0ebc67ba366233e572ee00d42390ec9c745c1532
kleber <kleber099@gmail.com>
2019-03-25 21:25:57 -0300

    Preparando a estrutura do projeto

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>
+      Copyright &copy; 2019
+      <a href='https://github.com/kleber099' target='_blank'> Kleber Mesquita</a>
+    </strong>
+  </footer>
+)
